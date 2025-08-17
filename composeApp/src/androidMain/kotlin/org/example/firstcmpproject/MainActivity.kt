56bfc7a232f3626d6d29688c14fc4aef0c5e080d
Shine Aung Khant <shineaungkhant1@gmail.com>
2025-08-17 17:15:57 +0700

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/org/example/firstcmpproject/MainActivity.kt
@@ -0,0 +1,29 @@
+package org.example.firstcmpproject
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+        )
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            App()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    App()
+}
